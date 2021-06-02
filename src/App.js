c2b11f8bf4c4867c8f321ec27b87e10ea9001cb2
sanajitjana <sanajitjana1402@gmail.com>
2021-06-03 02:32:51 +0530

    add add-btn and less-btn

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
+  // useState
+  const state = useState();
+  const [count, setCount] = useState(0);
+
+  const IncNum = () => {
+    setCount(count + 1);
+  };
+
+  const DecNum = () => {
+    setCount(count - 1);
+  };
+
   return (
     <>
-      <div className="text-center" style={{ marginTop: "50vh" }}>
-        <h1>0</h1>
-        <button type="button" className="btn btn-primary btn-xl">
-          Click Me
+      <div className="text-center" style={{ marginTop: "40vh" }}>
+        <h1>{count}</h1>
+        <br />
+        <button
+          onClick={IncNum}
+          type="button"
+          className="btn btn-primary btn-xl"
+          style={{ marginRight: "15px" }}
+        >
+          Add
+        </button>
+        <button
+          onClick={DecNum}
+          type="button"
+          className="btn btn-primary btn-xl"
+        >
+          Less
         </button>
       </div>
     </>
