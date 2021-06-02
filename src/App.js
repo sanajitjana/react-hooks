5691d9c363b4517798807e846d90f14c97dcaf43
sanajitjana <sanajitjana1402@gmail.com>
2021-06-03 01:48:27 +0530

    add button and h1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="text-center" style={{ marginTop: "50vh" }}>
+        <h1>0</h1>
+        <button type="button" className="btn btn-primary btn-xl">
+          Click Me
+        </button>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
